8cea4989882c0db91cbcc729dffad52d294a539a
filiperochs <filipesilva.tmsi@gmail.com>
2023-03-22 09:28:58 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -27,21 +27,7 @@ public class Customer {
       Rental each = (Rental) rentals.nextElement();
 
       // determine amounts for each line
-      switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-      }
+      thisAmount = amountFor(each);
 
       // add frequent renter points
       frequentRenterPoints++;
@@ -61,5 +47,27 @@ public class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints) +
         " frequent renter points";
     return result;
+  }
+
+  private double amountFor(Rental each) {
+    double amount = 0;
+
+    switch (each.getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        amount += 2;
+        if (each.getDaysRented() > 2)
+          amount += (each.getDaysRented() - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        amount += each.getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        amount += 1.5;
+        if (each.getDaysRented() > 3)
+          amount += (each.getDaysRented() - 3) * 1.5;
+        break;
+    }
+
+    return amount;
   }
 }
