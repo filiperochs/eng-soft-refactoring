bfab7d698e2336708405c7e7dd95897bc94817fc
filiperochs <filipesilva.tmsi@gmail.com>
2023-03-22 10:17:50 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -1,26 +1,48 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+  public static final int CHILDRENS = 2;
+  public static final int REGULAR = 0;
+  public static final int NEW_RELEASE = 1;
 
   private String _title;
   private int _priceCode;
 
   public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+    _title = title;
+    _priceCode = priceCode;
   }
 
   public int getPriceCode() {
-      return _priceCode;
+    return _priceCode;
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+    _priceCode = arg;
   }
 
-  public String getTitle (){
-      return _title;
+  public String getTitle() {
+    return _title;
+  }
+
+  public double getCharge(int daysRented) {
+    double amount = 0;
+
+    switch (getPriceCode()) {
+      case Movie.REGULAR:
+        amount += 2;
+        if (daysRented > 2)
+          amount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        amount += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        amount += 1.5;
+        if (daysRented > 3)
+          amount += (daysRented - 3) * 1.5;
+        break;
+    }
+
+    return amount;
   }
 }
--- Rental.java
+++ Rental.java
@@ -17,25 +17,7 @@ public class Rental {
   }
 
   public double getCharge() {
-    double amount = 0;
-
-    switch (getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        amount += 2;
-        if (getDaysRented() > 2)
-          amount += (getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        amount += getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        amount += 1.5;
-        if (getDaysRented() > 3)
-          amount += (getDaysRented() - 3) * 1.5;
-        break;
-    }
-
-    return amount;
+    return _movie.getCharge(_daysRented);
   }
 
   public int getFrequentRenterPoints() {
