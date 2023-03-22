d89d31e0eae5044a22fde34e138b3971771c6f30
filiperochs <filipesilva.tmsi@gmail.com>
2023-03-22 09:53:37 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -37,4 +37,13 @@ public class Rental {
 
     return amount;
   }
+
+  public int getFrequentRenterPoints() {
+    // add bonus for a two day new release rental
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        getDaysRented() > 1)
+      return 2;
+
+    return 1;
+  }
 }
